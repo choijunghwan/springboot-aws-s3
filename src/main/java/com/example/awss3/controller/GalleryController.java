f968603bdb07664c75759b40e87eba993e876693
ChoiJungHwan <vkdlxj3562@naver.com>
2021-12-08 16:56:07 +0900

    기본 환경 세팅
    * MySQL DB 연결 설정
    * gallery.html 파일 업로드 뷰 페이지 생성
    * GalleyController : gallery 페이지 맵핑

--- src/main/java/com/example/awss3/controller/GalleryController.java
+++ src/main/java/com/example/awss3/controller/GalleryController.java
@@ -1,5 +1,15 @@
 package com.example.awss3.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class GalleryController {
 
+    @GetMapping("/gallery")
+    public String dispWrite() {
+        return "/gallery";
+    }
 }
