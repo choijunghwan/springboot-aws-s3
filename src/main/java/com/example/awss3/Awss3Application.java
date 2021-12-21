e508cc259ef0cb0a2a261aa06a0b9aa10c67a2ef
ChoiJungHwan <vkdlxj3562@naver.com>
2021-12-21 23:49:46 +0900

    ãaws crediential 설정 yml 파ì ¼ 분리

--- src/main/java/com/example/awss3/Awss3Application.java
+++ src/main/java/com/example/awss3/Awss3Application.java
@@ -2,12 +2,19 @@ package com.example.awss3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Awss3Application {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+            "classpath:application.yml," +
+            "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Awss3Application.class, args);
+        new SpringApplicationBuilder(Awss3Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
